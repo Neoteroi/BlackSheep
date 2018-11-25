f9b3c0d56a215ebde6402a775b2d853fa8e6a858
Roberto Prevato <roberto.prevato@gmail.com>
2018-11-25 13:55:28 +0100

    labor limae

--- profiling.sh
+++ profiling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $1 -eq 1 ]
 then
-echo enable.
+echo enable
 find ./blacksheep/*.pxd -maxdepth 1 -type f -exec sed -i ' 1 s/.*/&, profile=True/' {} \;
 find ./blacksheep/*.pyx -maxdepth 1 -type f -exec sed -i '1s;^;# cython: profile=True\n;' {} \;
 else

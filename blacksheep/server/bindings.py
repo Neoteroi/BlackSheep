fecbd10f9a442f3f44c11aebc8b0c2957e80a900
Roberto Prevato <roberto.prevato@gmail.com>
2019-05-09 17:18:29 +0200

    wip on bindings

--- blacksheep/server/bindings.py
+++ blacksheep/server/bindings.py
@@ -1,39 +1,129 @@
-from typing import Type
+from abc import ABC, abstractmethod
+from typing import TypeVar, Optional, Callable, List
+from urllib.parse import unquote
 from blacksheep import Request
 from blacksheep.exceptions import BadRequest
 
 
-class FromBody:
+T = TypeVar('T')
+
+
+def _generalize_init_type_error_message(ex: TypeError) -> str:
+    return str(ex)\
+        .replace('__init__() ', '')\
+        .replace('keyword argument', 'parameter') \
+        .replace('keyword arguments', 'parameters') \
+        .replace('positional arguments', 'parameters')\
+        .replace('positional argument', 'parameter')
+
+
+class Binder(ABC):
 
     def __init__(self,
-                 expected_type: Type,
-                 required: bool = False,
-                 accept_json: bool = True,
-                 accept_xml: bool = True):
+                 expected_type: T,
+                 required: bool = True,
+                 converter: Optional[Callable] = None):
         self.expected_type = expected_type
         self.required = required
-        self.accept_json = accept_json
-        self.accept_xml = accept_xml
+        self.converter = converter
 
-    async def get_json_value(self, request: Request):
-        data = await request.json()
-
-        try:
-            return self.expected_type(**data)
-        except TypeError as te:
-            # TODO: log this error maybe
-            # TODO: '__init__() got an unexpected keyword argument \'c\'' maybe should be ignored!
-            raise BadRequest('Invalid request body')
-
-    async def get_xml_value(self, request: Request):
+    @abstractmethod
+    async def get_value(self, request: Request) -> T:
         pass
 
-    async def get_value(self, request: Request):
 
+class MissingBodyError(BadRequest):
+
+    def __init__(self):
+        super().__init__('Missing body payload')
+
+
+class MissingParameterError(BadRequest):
+
+    def __init__(self, name: str, source: str):
+        super().__init__(f'Missing parameter `{name}` from {source}')
+
+
+class InvalidRequestBody(BadRequest):
+
+    def __init__(self, description: Optional[str] = 'Invalid body payload'):
+        super().__init__(description)
+
+
+class FromJson(Binder):
+
+    def __init__(self,
+                 expected_type: T,
+                 required: bool = False,
+                 converter: Optional[Callable] = None
+                 ):
+        super().__init__(expected_type, required, converter)
+
+    def parse_value(self, data: dict) -> T:
+        try:
+            if self.converter:
+                return self.converter(data)
+
+            return self.expected_type(**data)
+        except TypeError as te:
+            raise InvalidRequestBody(_generalize_init_type_error_message(te))
+        except ValueError as ve:
+            raise InvalidRequestBody(str(ve))
+
+    async def get_value(self, request: Request) -> T:
         if request.declares_json():
-            return await self.get_json_value(request)
+            data = await request.json()
 
-        if request.declares_xml():
-            return await self.get_xml_value(request)
+            if not data:
+                raise MissingBodyError()
+
+            return self.parse_value(data)
+
+        if self.required:
+            if not request.has_body():
+                raise MissingBodyError()
+
+            raise InvalidRequestBody('Expected JSON payload')
 
         return None
+
+
+class FromHeader(Binder):
+
+    def __init__(self,
+                 name: bytes,
+                 expected_type: T,
+                 required: bool = False,
+                 converter: Optional[Callable] = None):
+        super().__init__(expected_type, required, converter)
+        self.name = name
+
+    def parse_value(self, value: List[bytes]) -> T:
+        if not value:
+            return None
+
+        if self.converter:
+            return self.converter(value)
+
+        # TODO: unquote the value because it's more user-friendly
+        # TODO: support list of strings and unquote them
+        if self.expected_type is str:
+            return value[0].decode()
+
+        if self.expected_type is bytes:
+            return value[0]
+
+        if isinstance(value, type(self.expected_type)):
+            return value
+
+    async def get_value(self, request: Request) -> T:
+        headers = request.headers[self.name]
+
+        value = self.parse_value([header.value for header in headers])
+
+        if not value:
+            if self.required:
+                raise MissingParameterError(self.name.decode(), 'header')
+            return None
+
+        return value
--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -1,6 +1,7 @@
 import pytest
+from pytest import raises
 from blacksheep import Request, Headers, Header, JsonContent
-from blacksheep.server.bindings import FromBody
+from blacksheep.server.bindings import FromJson, FromHeader, InvalidRequestBody
 
 
 JsonContentType = Header(b'Content-Type', b'application/json')
@@ -10,6 +11,13 @@ XmlContentType = Header(b'Content-Type', b'application/xml')
 class ExampleOne:
 
     def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+class ExampleTwo:
+
+    def __init__(self, a, b, **kwargs):
         self.a = a
         self.b = b
 
@@ -24,7 +32,50 @@ async def test_from_body_json_binding():
         'b': 9000
     }))
 
-    parameter = FromBody(ExampleOne)
+    parameter = FromJson(ExampleOne)
+
+    value = await parameter.get_value(request)
+
+    assert isinstance(value, ExampleOne)
+    assert value.a == 'world'
+    assert value.b == 9000
+
+
+@pytest.mark.asyncio
+async def test_from_body_json_binding_extra_parameters_strategy():
+
+    request = Request(b'POST', b'/', Headers([
+        JsonContentType
+    ]), JsonContent({
+        'a': 'world',
+        'b': 9000,
+        'c': 'This is an extra parameter, accepted by constructor explicitly'
+    }))
+
+    parameter = FromJson(ExampleTwo)
+
+    value = await parameter.get_value(request)
+
+    assert isinstance(value, ExampleTwo)
+    assert value.a == 'world'
+    assert value.b == 9000
+
+
+@pytest.mark.asyncio
+async def test_from_body_json_with_converter():
+
+    request = Request(b'POST', b'/', Headers([
+        JsonContentType
+    ]), JsonContent({
+        'a': 'world',
+        'b': 9000,
+        'c': 'This is an extra parameter, accepted by constructor explicitly'
+    }))
+
+    def convert(data):
+        return ExampleOne(data.get('a'), data.get('b'))
+
+    parameter = FromJson(ExampleOne, converter=convert)
 
     value = await parameter.get_value(request)
 
@@ -41,7 +92,7 @@ async def test_from_body_json_binding_request_missing_content_type():
         'b': 9000
     }))
 
-    parameter = FromBody(ExampleOne)
+    parameter = FromJson(ExampleOne)
 
     value = await parameter.get_value(request)
 
@@ -58,10 +109,21 @@ async def test_from_body_json_binding_invalid_input():
         'd': 2
     }))
 
-    parameter = FromBody(ExampleOne)
+    parameter = FromJson(ExampleOne)
+
+    with raises(InvalidRequestBody, message="got an unexpected parameter 'c'"):
+        await parameter.get_value(request)
+
+
+@pytest.mark.asyncio
+async def test_from_header_binding():
+    request = Request(b'GET', b'/', Headers([
+        Header(b'X-Foo', b'Foo')
+    ]), None)
+
+    parameter = FromHeader(b'X-Foo', str)
 
     value = await parameter.get_value(request)
 
-    assert isinstance(value, ExampleOne)
-    assert value.a == 'world'
-    assert value.b == 9000
+    assert isinstance(value, str)
+    assert value == 'Foo'

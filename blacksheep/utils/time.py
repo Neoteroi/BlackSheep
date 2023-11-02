428548c465a639ff4ae5859340b3296af90af622
bymoye <964907582@qq.com>
2023-11-02 10:33:57 +0800

    use: utc

--- blacksheep/utils/time.py
+++ blacksheep/utils/time.py
@@ -9,4 +9,4 @@ MIN_DATETIME = datetime(MINYEAR, 1, 1, tzinfo=None)
 def utcnow() -> datetime:
     if sys.version_info < (3, 12):
         return datetime.utcnow()
-    return datetime.now(UTC).astimezone(None).replace(tzinfo=None)
+    return datetime.now(UTC).replace(tzinfo=None)

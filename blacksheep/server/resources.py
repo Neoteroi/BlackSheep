a91205bb61a84ffd0153ac562de122de6fc7acf4
Roberto Prevato <roberto.prevato@gmail.com>
2023-11-03 18:39:07 +0100

    Restore compatibility with Python 3.8

--- blacksheep/server/resources.py
+++ blacksheep/server/resources.py
@@ -1,8 +1,20 @@
-from importlib.resources import files
+"""
+This module offers methods to return file paths for resources. Its original purpose is
+to provide contents of static files stored for conveniency in the blacksheep.server.res
+package.
+"""
 
+try:
+    from importlib.resources import files
 
-def get_resource_file_path(anchor, file_name: str) -> str:
-    return str(files(anchor) / file_name)
+    def get_resource_file_path(anchor, file_name: str) -> str:
+        return str(files(anchor) / file_name)
+except ImportError:
+    # Python 3.8
+    import pkg_resources
+
+    def get_resource_file_path(anchor, file_name: str) -> str:
+        return pkg_resources.resource_filename(anchor, file_name)
 
 
 def get_resource_file_content(file_name: str) -> str:

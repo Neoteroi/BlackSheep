36d17cdf6aa833a9b0e9e7e2e9e3f4cfef912e71
Roberto Prevato <roberto.prevato@gmail.com>
2023-12-18 18:40:58 +0100

    Fix #450
    * Fix #450
    
    * Update CHANGELOG.md

--- blacksheep/__init__.py
+++ blacksheep/__init__.py
@@ -3,7 +3,7 @@ Root module of the framework. This module re-exports the most commonly
 used types to reduce the verbosity of the imports statements.
 """
 __author__ = "Roberto Prevato <roberto.prevato@gmail.com>"
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 from .contents import Content as Content
 from .contents import FormContent as FormContent

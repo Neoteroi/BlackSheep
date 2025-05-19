2a1f1f2e32d5553f23130ebff50851d89ca9c37a
Roberto Prevato <roberto.prevato@gmail.com>
2025-05-19 07:30:51 +0200

    PyPy workflow

--- blacksheep/__init__.py
+++ blacksheep/__init__.py
@@ -4,7 +4,7 @@ used types to reduce the verbosity of the imports statements.
 """
 
 __author__ = "Roberto Prevato <roberto.prevato@gmail.com>"
-__version__ = "2.3.1"
+__version__ = "2.3.1a1"
 
 from .contents import Content as Content
 from .contents import FormContent as FormContent

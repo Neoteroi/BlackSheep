cff623b9843225404ab075f3f2d05da3e2cb7e82
Roberto Prevato <roberto.prevato@gmail.com>
2025-10-19 08:39:39 +0200

    Fix full support for Python 3.14

--- itests/conftest.py
+++ itests/conftest.py
@@ -1,15 +1,16 @@
-import asyncio
 import os
 
 import pytest
 
+from blacksheep.utils.aio import get_running_loop
 from itests.client_fixtures import *
 
 os.environ["APP_DEFAULT_ROUTER"] = "false"
 
 
+# async is needed here, to use the same event loop
 @pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.get_event_loop()
+async def event_loop():
+    loop = get_running_loop()
     yield loop
     # pytest-asyncio closes the loop and would complain if the loop was closed here.
